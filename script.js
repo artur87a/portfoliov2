5ce86720cf92b0d1094dee074cfc6bb9c03163ae
artur.kodehode@gmail.com <artur.kodehode@gmail.com>
2023-09-20 10:47:03 +0200

    update 20.09

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("slide").style.marginLeft = "250px";
-  
+  // document.getElementById("mySidebar").style.width = "250px";
+  // document.getElementById("slide").style.marginLeft = "250px";
+  // document.getElementById("mySidebar").style.transform = "translateX(-100%)";
+ document.getElementById("mySidebar").style.transform = "translateX(0px)"
+// element.style.transform = "transformX(100%)"
 }
 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("slide").style.marginLeft= "0";
-}
+  // document.getElementById("mySidebar").style.width = "0";
+  // document.getElementById("slide").style.marginLeft= "0";
+document.getElementById("mySidebar").style.transform = "translateX(-250px)"}
+
